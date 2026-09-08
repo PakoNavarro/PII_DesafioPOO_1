50fae8065d74a7ba984fd9362b0141b64bb7f9d3
PakoNavarro <122231574+PakoNavarro@users.noreply.github.com>
2026-09-08 15:31:06 -0300

    Initial commit

---
+++ src/Library/Circle.cs
@@ -0,0 +1,10 @@
+﻿//--------------------------------------------------------------------------------
+// <copyright file="Circle.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
+namespace Ucu.Poo.Desafios
+{
+    // Agrega tu clase Circle aquí.
+}
---
+++ src/Program/Program.cs
@@ -0,0 +1,26 @@
+﻿//--------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
+using System;
+
+namespace Ucu.Poo.Desafios
+{
+    /// <summary>
+    /// Programa principal.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Punto de entrada al programa principal.
+        /// </summary>
+        public static void Main()
+        {
+            // Agrega aquí un ejemplo que muestre la creación de un círculo y la
+            // impresión en la consola de su circunferencia y su área.
+            Console.WriteLine("Reemplaza esto por tu ejemplo");
+        }
+    }
+}
